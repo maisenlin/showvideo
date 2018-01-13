5bec95c2ca711719d665feff2180fc6130238500
snlinMai <maisenlin@yeah.net>
2018-01-13 13:27:39 +0800

    init

---
+++ routes/index.js
@@ -0,0 +1,28 @@
+var path = require("path");  
+var express = require("express");  
+var router = express.Router();  
+  
+router.get("/",function(req,res){  
+    //res.sendFile(path.join(__dirname,"upload.html"));  
+    res.send("OK");  
+});  
+  
+router.get("/user",function(req,res){  
+    res.send("index user page" + req.url);  
+});  
+  
+router.get("/admin",function(req,res){  
+    res.send("admin page" + req.url);  
+});  
+  
+router.get("/redirect",function(req,res){  
+    res.redirect("https://www.baidu.com");  
+});  
+  
+router.get("/upload",function(req,res){  
+  // res.send(path.join(__dirname,"../upload.html"));  
+  console.log("upload.html");  
+  res.sendFile(path.join(__dirname,"../upload.html"));  
+  
+});  
+module.exports = router;
